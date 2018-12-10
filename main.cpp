f33376167a5fbc01c0b99067ef4becce42888dd9
BuildTools <gandalfphysicist@gmail.com>
2018-12-10 15:52:57 -0600

    Testing MinGW Transfer

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "Chemistry.h"
+#include "Telescience.h"
+#include "Food.h"
+#include "Bar.h"
+#include <iostream>
+using namespace std;
+
+string cmd;
+
+void init(){
+    cout << "SS13 Program Hub v1.0" << endl << "> ";
+    getline(cin,cmd);
+    if(cmd=="chem"){
+        Chemistry chem;
+        chem.init();
+        init();
+    }else if(cmd=="sci"){
+        Telescience sci;
+        sci.init();
+        init();
+    }else if(cmd=="food"){
+        Food food;
+        food.init();
+        init();
+    }else if(cmd=="bar"){
+        Bar bar;
+        bar.init();
+        init();
+    }else if(cmd=="help"){
+        cout << "chem: opens chemistry wiki" << endl
+        << "sci: opens telescience console" << endl
+        << "food: opens food wiki" << endl
+        << "bar: opens drinks wiki" << endl;
+        init();
+    }else if(cmd=="exit"||cmd=="e"){
+    }else{
+        cout << "Invalid Command\n";
+        init();
+    }
+}
+
+int main() {
+    init();
+}
